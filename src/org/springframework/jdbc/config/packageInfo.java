69f295ff2f10f90fbc0e79f52dce75fcc8cafc62
Keunoh <rmsdhcjswo1@gmail.com>
2023-04-06 23:08:50 +0900

    package-info interface

---
+++ src/org/springframework/jdbc/config/packageInfo.java
@@ -0,0 +1,11 @@
+/**
+ * Defines the Spring JDBC configuration namespace.
+ */
+
+@NonNullApi
+@NonNullFields
+package org.springframework.jdbc.config;
+
+import org.springframework.lang.NonNullApi;
+import org.springframework.lang.NonNullFields;
+
