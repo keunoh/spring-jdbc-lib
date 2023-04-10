46d19979dadc1f348f750527976cac55c0e53356
Keunoh <rmsdhcjswo1@gmail.com>
2023-04-10 23:58:44 +0900

    GenericSqlQuery

---
+++ src/org/springframework/jdbc/object/GenericSqlQuery.java
@@ -0,0 +1,55 @@
+package org.springframework.jdbc.object;
+
+
+import javax.swing.tree.RowMapper;
+import java.util.Map;
+
+/**
+ * A concrete variant of SqlQuery which can be configured with a RowMapper
+ *
+ * @param <T>
+ */
+public class GenericSqlQuery<T> extends SqlQuery<T> {
+
+    @Nullable
+    private RowMapper<T> rowMapper;
+
+    @SuppressWarnings("rawtypes")
+    @Nullable
+    private Class<? extends RowMapper> rowMapperClass;
+
+    /**
+     * Set a specific RowMapper instance to use for this query.
+     */
+    public void setRowMapper(RowMapper<T> rowMapper) {
+        this.rowMapper = rowMapper;
+    }
+
+    /**
+     * Set a RowMapper class for this query, creating a fresh RowMapper instance per execution.
+     */
+    @SuppressWarnings("rawtypes")
+    @Nullable
+    public void setRowMapperClass(Class<? extends RowMapper> rowMapperClass) {
+        this.rowMapperClass = rowMapperClass;
+    }
+
+    @Override
+    public void afterPropertiesSet() {
+        super.afterProperteisSet();
+        Assert.isTrue(this.rowMapper != null || this.rowMapperClass != null,
+                "'rowMapper' or 'rowMapperClass' is required");
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    protected RowMapper<T> newRowMapper(@Nullable Object[] parameters, @Nullable Map<?, ?> context) {
+        if (this.rowMapper != null) {
+            return this.rowMapper;
+        }
+        else {
+            Assert.state(this.rowMapperClass != null, "No RowMapper set");
+            return BeanUtils.instantiateClass(this.rowMapperClass);
+        }
+    }
+}
