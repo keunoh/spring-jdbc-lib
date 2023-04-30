c0bf5da34bf719714b44c9543d0e4e7944b17173
Keunoh <rmsdhcjswo1@gmail.com>
2023-04-30 23:13:55 +0900

    StoredProcedure

---
+++ src/org/springframework/jdbc/object/StoredProcedure.java
@@ -0,0 +1,57 @@
+package org.springframework.jdbc.object;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class StoredProcedure extends SqlCall {
+
+    protected StoredProcedure() {
+
+    }
+
+    protected StoredProcedure(DataSource ds, String name) {
+        setDataSource(ds);
+        setSql(name);
+    }
+
+    protected StoredProcedure(JdbcTemplate jdbcTemplate, String name) {
+        setJdbcTemplate(jdbcTemplate);
+        setSql(name);
+    }
+
+    @Override
+    protected boolean allowsUnusedParameters() {
+        return true;
+    }
+
+    @Override
+    public void declareParameter(SqlParamter param) throws InvalidDataAccessApiUsageException {
+        if (param.getName() == null) {
+            throw new InvalidDataAccessApiUsageException("Parameters to stored procedures must have names as well as types");
+        }
+        super.declareParameter(param);
+    }
+
+    public Map<String, Object> execute(Object... inParams) {
+        Map<String, Object> paramsToUse = new HashMap<>();
+        validateParameters(inParams);
+        int i = 0;
+        for (SqlParamter sqlParamter : getDeclareParameters()) {
+            if (sqlParamter.isInputValueProvided() && i < inParams.length) {
+                paramsToUse.put(sqlParamter.getName(), inParams[i++]);
+            }
+        }
+        return getJdbcTemplate().call(newCallableStatementCreator(paramsToUse), getDeclareParameters());
+    }
+
+    public Map<String, Object> execute(Map<String, ?> inParams) throws DataAccessException {
+        validateParameters(inParams.values().toArray());
+        return getJdbcTemplate().call(newCallableStatementCreator(inParams), getDeclareParameters());
+    }
+
+    public Map<String, Object> execute(ParameterMapper inParamMapper) throws DataAccessException {
+        checkCompiled();
+        return getJdbcTemplate().call(newCallableStatementCreator(inParamMapper), getDeclareParameters());
+    }
+}
