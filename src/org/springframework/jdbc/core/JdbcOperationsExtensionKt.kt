109602d3b22b4cc787de9b049c78d34aaaa447ff
Keunoh <rmsdhcjswo1@gmail.com>
2023-05-06 23:32:21 +0900

    JdbcOperationsExtensionKt in progress

---
+++ src/org/springframework/jdbc/core/JdbcOperationsExtensionKt.kt
@@ -0,0 +1,3 @@
+package org.springframework.jdbc.core
+
+public inline fun <reified T> org.springframework.jdbc.core.JdbcOperations.query(sql: kotlin.String, vararg args: kotlin.Any, crossinline function: (java.sql.ResultSet) -> T): T { /* compiled code */ }
