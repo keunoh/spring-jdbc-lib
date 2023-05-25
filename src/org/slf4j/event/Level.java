cf8a6a86f7f6191d1dc219976547583e5b243056
Keunoh <rmsdhcjswo1@gmail.com>
2023-05-25 23:17:14 +0900

    Enum level

---
+++ src/org/slf4j/event/Level.java
@@ -0,0 +1,28 @@
+package org.slf4j.event;
+
+import static org.slf4j.event.EventConstants.DEBUG_INT;
+import static org.slf4j.event.EventConstants.ERROR_INT;
+import static org.slf4j.event.EventConstants.INFO_INT;
+import static org.slf4j.event.EventConstants.TRACE_INT;
+import static org.slf4j.event.EventConstants.WARN_INT;
+
+public enum Level {
+
+    ERROR(ERROR_INT, "ERROR"), WARN(WARN_INT, "WARN"), INFO(INFO_INT, "INFO"), DEBUG(DEBUG_INT, "DEBUG"), TRACE(TRACE_INT, "TRACE");
+
+    private int levelInt;
+    private String levelStr;
+
+    Level(int i, String s) {
+        this.levelInt = i;
+        this.levelStr = s;
+    }
+
+    public int toInt() {
+        return levelInt;
+    }
+
+    public String toString() {
+        return levelStr;
+    }
+}
